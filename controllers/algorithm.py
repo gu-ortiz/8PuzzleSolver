3de1f6b174919d5afbfb89de575fd8dff19d84b4
Gustavo Ortiz <gustavo.egert.ortiz@gmail.com>
2022-05-18 00:22:37 -0300

    changed all structure, need to fix frontiers board insert

--- controllers/algorithm.py
+++ controllers/algorithm.py
@@ -2,21 +2,13 @@ from entities.move import Move
 from entities.board import Board
 
 
-def position_length_to_destiny(move: Move, board: Board, result_board: Board) -> int:
-    new_board = board.apply_move_to_board(move)
-    coordenate_x, coordenate_y = move.origin.coordenate.get_axis()
-    expected_x, expected_y = result_board.find_position_by_value(move.destiny.value).coordenate.get_axis()
+def uniform_cost(board: Board, result_board: Board) -> int:
+    return len(board.path)
 
-    return (
-        abs(coordenate_x - expected_x) +
-        abs(coordenate_y - expected_y)
-    )
-
-
-def board_length_to_destiny(move: Move, board: Board, result_board: Board) -> int:
+def position_length_to_destiny(board: Board, result_board: Board) -> int:
     heuristic_value = 1
-    new_board = board.apply_move_to_board(move)
-    for line in new_board.matrix:
+
+    for line in board.matrix:
         for position in line:
             if position.value != 0:
                 coordenate_x, coordenate_y = position.coordenate.get_axis()
@@ -26,4 +18,10 @@ def board_length_to_destiny(move: Move, board: Board, result_board: Board) -> in
                     abs(coordenate_y - expected_y)
                 )
 
-    return heuristic_value + position_length_to_destiny(move, board, result_board)
+    return (
+        heuristic_value +
+        uniform_cost(board, result_board)
+    )
+
+def board_length_to_destiny(board: Board, result_board: Board) -> int:
+    return
--- controllers/puzzle_solver.py
+++ controllers/puzzle_solver.py
@@ -1,4 +1,4 @@
-from controllers.algorithm import board_length_to_destiny, position_length_to_destiny
+from controllers.algorithm import board_length_to_destiny, position_length_to_destiny, uniform_cost
 from entities.position import Position
 from entities.board import Board
 from entities.move import Move
@@ -6,66 +6,67 @@ from entities.move import Move
 
 class PuzzleSolver:
     def __init__(self) -> None:
-        self.__border: list[Board] = []
-        self.__visited_nodes: list[Board] = []
-        self.__total_nodes_created = 0
-        self.__border_highest_size = 0
+        self.__frontier: list[Board] = []
+        self.__visited: list[Board] = []
+        self.__total_boards_created = 0
+        self.__frontier_highest_size = 0
 
     def create_board(self, starting_matrix: list[list[Position]], result_matrix: list[list[Position]]) -> tuple[Board, Board]:
         return Board(starting_matrix), Board(result_matrix)
 
-    def get_best_expansion(self, result_board: Board, algorithm: callable):
-        best_expansion: (tuple[Move, Board] | None) = None
+    def get_best_expansion(self, result_board: Board, algorithm: callable) -> (Board | None):
+        best_expansion: (Board | None) = None
 
-        for board in self.__border:
-            if len(board.moves) == 0:
-                self.__border.remove(board)
-                continue
-
-            for move in board.moves:
-                if not best_expansion:
-                    best_expansion = (move, board)
-                else:
-                    best_expansion = (move, board) if (
-                        algorithm(move, board, result_board) < algorithm(best_expansion[0], best_expansion[1], result_board)
-                    ) else best_expansion
-
-        if best_expansion:
-            best_expansion[1].moves.remove(best_expansion[0])
+        for board in self.__frontier:
+            if not best_expansion:
+                best_expansion = board
+            else:
+                best_expansion = board if (
+                    algorithm(board, result_board) < algorithm(best_expansion, result_board)
+                ) else best_expansion
 
         return best_expansion
 
-    def expand_frontier(self, move_board_tuple: tuple[Move, Board]) -> None:
-        move, board = move_board_tuple
-        new_board = board.apply_move_to_board(move)
-        new_board.moves = new_board.get_possible_moves()
+    def check_board_in_list(self, board: Board, list: list[Board]) -> bool:
+        for item in list:
+            if item.get_board_string() == board.get_board_string():
+                return True
 
-        if new_board.get_board_string() not in self.__visited_nodes:
-            self.__border.append(new_board)
-            self.__visited_nodes.append(new_board.get_board_string())
-            self.__total_nodes_created += 1
+        return False
 
-    def check_for_win_condition(self, result_board: Board) -> bool:
-        if result_board.get_board_string() == self.__border[-1].get_board_string():
+    def check_for_win_condition(self, board: Board, result_board: Board) -> bool:
+        if board.get_board_string() == result_board.get_board_string():
             return True
 
         return False
 
-    def compose_result_variables(self) -> None:
-        pass
+    def visit_board(self, board: Board, result_board: Board) -> None:
+        self.__frontier = [item for item in self.__frontier if item.get_board_string() != board.get_board_string()]
+        self.__visited.append(board)
 
-    def resolve_board(self, result_board: Board, algorithm=position_length_to_destiny) -> None:
+        if self.check_for_win_condition(board, result_board):
+            return True
+
+        for move in board.moves:
+            new_board = board.apply_move_to_board(move)
+            new_board.moves = new_board.get_possible_moves()
+            if not self.check_board_in_list(board, self.__frontier) and not self.check_board_in_list(board, self.__visited):
+                self.__frontier.append(new_board)
+                self.__total_boards_created += 1
+
+        return False
+
+    def resolve_board(self, result_board: Board, algorithm: callable) -> Board:
         while True:
-            if len(self.__border) > self.__border_highest_size:
-                self.__border_highest_size = len(self.__border)
+            if len(self.__frontier) > self.__frontier_highest_size:
+                self.__frontier_highest_size = len(self.__frontier)
 
-            if self.check_for_win_condition(result_board):
+            board = self.get_best_expansion(result_board, algorithm)
+
+            if self.visit_board(board, result_board):
                 break
 
-            expansion = self.get_best_expansion(result_board, algorithm)
-
-            if expansion:
-                self.expand_frontier(expansion)
+        return board
 
     def print_solution(self, board: Board) -> None:
         print('\nCaminho:\n')
@@ -78,23 +79,21 @@ class PuzzleSolver:
             ']]', ']').replace(' ', '').replace('0', ' ') + '\n')
 
         print('Tamanho...........', len(board.path))
-        print('Nodos visitados...', len(self.__visited_nodes))
-        print('Nodos criados.....', self.__total_nodes_created)
-        print('Maior fronteira...', self.__border_highest_size)
+        print('Nodos visitados...', len(self.__visited))
+        print('Nodos criados.....', self.__total_boards_created)
+        print('Maior fronteira...', self.__frontier_highest_size)
 
     def run_solution(self, payload: dict) -> None:
         algorithm_dispatch = {
-            1: position_length_to_destiny,
-            2: board_length_to_destiny,
-            3: None
+            1: uniform_cost,
+            2: position_length_to_destiny,
+            3: board_length_to_destiny
         }
 
         starting_board, result_board = self.create_board(payload.get('starting_matrix'), payload.get('result_matrix'))
 
-        self.__border.append(starting_board)
-        self.__visited_nodes.append(starting_board)
-        self.__total_nodes_created += 1
+        self.__frontier.append(starting_board)
 
-        self.resolve_board(result_board, algorithm_dispatch[payload.get('algorithm')])
+        solution = self.resolve_board(result_board, algorithm_dispatch[payload.get('algorithm')])
 
-        self.print_solution(self.__border[-1])
+        self.print_solution(solution)
